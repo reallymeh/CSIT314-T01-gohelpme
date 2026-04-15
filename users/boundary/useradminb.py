2c81ff9969f4e418876b819dda46f71c95efd9cb
reallymeh <143067913+reallymeh@users.noreply.github.com>
2026-04-15 17:07:44 +0800

    Add: SuspendUserProfile class, blueprint route

--- users/boundary/useradminb.py
+++ users/boundary/useradminb.py
@@ -1,8 +1,8 @@
 from users.entity.user import User
 from users.entity.userprofile import UserProfile
-from users.control.useradminc import DisplayUserProfileController
+from users.control.useradminc import DisplayUserProfileController, SuspendUserProfileController
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 
 from typing import List
 
@@ -13,12 +13,36 @@ class DisplayUserProfile:
     def displayUserProfile(self) -> List[UserProfile]:
         return self.controller.displayUserProfile()
 
-# flask integration
+# flask integration for display user profiles
 admin_profiles_bp = Blueprint('admin_view_profile', __name__, url_prefix='/admin')
-
-display_profile = DisplayUserProfile()
 
 @admin_profiles_bp.route('/userprofile') # url will be .../admin/userprofile
 def user_profile_list():
+    display_profile = DisplayUserProfile()
     profiles = display_profile.displayUserProfile()
     return render_template('UserAdminProfiles.html', profiles=profiles)
+
+class SuspendUserProfile:
+    def __init__(self):
+        self.controller = SuspendUserProfileController()
+    
+    def displaySuspendSuccess(self):
+        return "Succesfully suspend user!"
+    
+    def displaySuspendFail(self):
+        return "Failed to suspend user!"
+
+    def suspendUserProfile(self, user_profile_name:str):
+        if self.controller.suspendUserProfile(user_profile_name):
+            return self.displaySuspendSuccess()
+        else:
+            return self.displaySuspendFail()
+
+@admin_profiles_bp.route('/suspend_user', methods=['POST'])
+def suspend_user():
+    data = request.get_json()
+    print(data)
+    user_profile_name = data.get('user_profile_name')
+    message = SuspendUserProfile().suspendUserProfile(user_profile_name)
+
+    return jsonify({'message': message})
