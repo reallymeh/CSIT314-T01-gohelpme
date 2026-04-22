3e5e08676395bd68f960ba33ddd6aa17c947f6ba
reallymeh <143067913+reallymeh@users.noreply.github.com>
2026-04-22 18:11:42 +0800

    [Add] FRACategory

---
+++ users/entity/fracategory.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from database import connect_db
+
+
+@dataclass
+class FRACategory:
+    category_name: str
+    description: str
+
+    @staticmethod
+    def createCategory(cat_name:str, description:str) -> bool:
+        if FRACategory.checkCategoryExist(cat_name):
+            return False
+        else:
+            conn, cur = connect_db()
+            try:
+                cur.execute(
+                    "INSERT INTO user_profile (name, description) VALUES (?, ?)",
+                    (cat_name, description)
+                )
+                conn.commit()
+                return True
+            except Exception as e:
+                print(e)
+                conn.rollback()
+                return False
+            finally:
+                cur.close()
+                conn.close()
+                
+    @staticmethod
+    def checkCategoryExist(name:str) -> bool:
+        conn, cur = connect_db()
+        cur.execute("SELECT 1 FROM fra_category WHERE name=? LIMIT 1", (name,))
+        
+        res = cur.fetchone()
+
+        cur.close()
+        conn.close()
+        
+        return res is not None
