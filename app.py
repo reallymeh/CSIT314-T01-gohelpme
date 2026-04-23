d8fd543f4fda247eff9e05afecbd6e8aef0e86f5
ophiuchi <hoojiasheng18@gmail.com>
2026-04-23 15:33:55 +0800

    added in user_bp

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from database import init_db
 from users.boundary.useradminb import admin_profiles_bp
-from users.boundary.userb import user_bp
 from users.boundary.platform_managerb import platform_manager_bp
+from users.boundary.userb import user_bp
 
 def create_app():
     app = Flask(__name__)
@@ -11,10 +11,10 @@ def create_app():
         init_db()
 
     app.register_blueprint(admin_profiles_bp)
-    app.register_blueprint(platform_manager_bp)  
+    app.register_blueprint(platform_manager_bp)
+    app.register_blueprint(user_bp)
 
     return app
-
 
 if __name__ == '__main__':
     app = create_app()
