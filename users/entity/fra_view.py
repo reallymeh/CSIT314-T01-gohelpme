9ea9c6d5d3034120e0d9756d7bc554d5fedbbaba
reallymeh <143067913+reallymeh@users.noreply.github.com>
2026-05-01 18:37:37 +0800

    [Add] Daily Report

--- users/entity/fra_view.py
+++ users/entity/fra_view.py
@@ -2,81 +2,132 @@ from dataclasses import dataclass
 from database import connect_db
 from typing import List
 
+from datetime import date
+
 
 @dataclass
 class FRAView:
-    id: int
-    fraId: str
-    viewer_email: str
-    view_date: str
-    fra_name: str
-    fra_category: str
+   id: int
+   fraId: str
+   viewer_email: str
+   view_date: str
+   fra_name: str
+   fra_category: str
 
-    @staticmethod
-    def recordView(fraId: str, viewer_email: str, owner_email: str, fra_name: str, fra_category: str) -> bool:
-     if not fraId or not viewer_email:
-        return False
+   @staticmethod
+   def getDailyCategoryView() -> List["FRAView"]:
+      conn, cur = connect_db()
 
-     if viewer_email == owner_email:
-        return False
+      today = date.today()
+      try:
+         cur.execute('''
+            SELECT fra_category, COUNT(*) AS total_view 
+            FROM fra_view
+            WHERE DATE(view_date) = DATE(?) 
+            GROUP BY fra_category
+            ''', (today,))
+         rows = cur.fetchall()
+         return [
+            {  
+                  "fra_category": r[0],
+                  "count": r[1],
+            }
+            for r in rows
+         ]
+      except Exception as e:
+         print(e)
+      finally:
+         cur.close()
+         conn.close()
 
-     try:
-        conn, cur = connect_db()
+   @staticmethod
+   def getDailyFRAView() -> int:
+      conn, cur = connect_db()
 
-        cur.execute("""
+      today = date.today()
+
+      try:
+         cur.execute('''
+            SELECT COUNT(*) AS TOTAL_VIEW FROM fra_view
+            WHERE DATE(view_date) = DATE(?)
+         ''', (today,))
+
+         row = cur.fetchone()
+         return row[0] if row else 0
+
+      except Exception as e:
+         print(e)
+      
+      finally:
+         cur.close()
+         conn.close()
+
+   @staticmethod
+   def recordView(fraId: str, viewer_email: str, owner_email: str, fra_name: str, fra_category: str) -> bool:
+      if not fraId or not viewer_email:
+         return False
+
+      if viewer_email == owner_email:
+         return False
+
+      try:
+         conn, cur = connect_db()
+
+         cur.execute("""
             INSERT OR IGNORE INTO fra_view 
             (fraId, user_email, fra_name, fra_category)
             VALUES (?, ?, ?, ?)
-        """, (fraId, viewer_email, fra_name, fra_category))
+         """, (fraId, viewer_email, fra_name, fra_category))
 
-        conn.commit()
-        return True
+         conn.commit()
+         return True
 
-     except Exception as e:
-        print(e)
-        return False
-     finally:
-        conn.close()
+      except Exception as e:
+         print(e)
+         return False
+      finally:
+         conn.close()
 
-    @staticmethod
-    def getViewStatsByDateAndUser(fraId: str) -> List[dict]:
-     if not fraId:
-        return []
+   @staticmethod
+   def getViewStatsByDateAndUser(fraId: str) -> List[dict]:
+      if not fraId:
+         return []
 
-     conn, cur = None, None
+      conn, cur = None, None
 
-     try:
-        conn, cur = connect_db()
+      try:
+         conn, cur = connect_db()
 
-        cur.execute("""
+         cur.execute("""
             SELECT user_email,
-                   DATE(view_date),
-                   COUNT(*),
-                   fra_name,
-                   fra_category
+                     DATE(view_date),
+                     COUNT(*),
+                     fra_name,
+                     fra_category
             FROM fra_view
             WHERE fraId = ?
             GROUP BY user_email, DATE(view_date), fra_name, fra_category
             ORDER BY DATE(view_date) DESC
-        """, (fraId,))
+         """, (fraId,))
 
-        rows = cur.fetchall()   
+         rows = cur.fetchall()   
 
-        return [
+         return [
             {  
-                "user_email": r[0],
-                "view_date": r[1],
-                "count": r[2],
-                "fra_name": r[3],
-                "fra_category": r[4]
+                  "user_email": r[0],
+                  "view_date": r[1],
+                  "count": r[2],
+                  "fra_name": r[3],
+                  "fra_category": r[4]
             }
             for r in rows
-        ]
+         ]
 
-     except Exception as e:
-        print(f"[FRAView.getViewStats] DB error: {e}")
-        return []
+      except Exception as e:
+         print(f"[FRAView.getViewStats] DB error: {e}")
+         return []
 
-     finally:
-        if conn:
+      finally:
+         if conn:
             conn.close()
+
